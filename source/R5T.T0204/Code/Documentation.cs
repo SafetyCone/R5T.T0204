8ad019db66c3fd216630eb6be45a81213b67217a
davidcoats <david@getsafetycone.com>
2023-05-28 11:33:40 -0700

    Initial commit.

---
+++ source/R5T.T0204/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0204
+{
+	/// <summary>
+	/// .NET project specification type.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0204/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.T0204
+.NET project specification type.
