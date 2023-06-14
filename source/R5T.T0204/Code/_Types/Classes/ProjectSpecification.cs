6dfff46b69d2b906c557837185c25d0db3c5f473
davidcoats <david@getsafetycone.com>
2023-06-14 00:33:38 -0700

    Interim changes.

--- source/R5T.T0204/Code/_Types/Classes/ProjectSpecification.cs
+++ source/R5T.T0204/Code/_Types/Classes/ProjectSpecification.cs
@@ -1,10 +1,12 @@
 using System;
 
+using R5T.T0142;
 using R5T.T0187;
 
 
 namespace R5T.T0204
 {
+    [DataTypeMarker]
     public class ProjectSpecification : IProjectSpecification
     {
         public IProjectName ProjectName { get; set; }
--- source/R5T.T0204/Code/_Types/Interfaces/IProjectSpecification.cs
+++ source/R5T.T0204/Code/_Types/Interfaces/IProjectSpecification.cs
@@ -1,10 +1,12 @@
 using System;
 
+using R5T.T0142;
 using R5T.T0187;
 
 
 namespace R5T.T0204
 {
+    [DataTypeMarker]
     public interface IProjectSpecification
     {
         IProjectName ProjectName { get; }
